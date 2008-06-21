6603f35dd055f6b1fb2636c296ef6a96b2e69ad3
Mike Hommey <glandium@debian.org>
2008-06-21 02:48:46 +0200

    Allow .js preference files to set locked prefs with lockPref()

--- modules/libpref/prefapi.cpp
+++ modules/libpref/prefapi.cpp
@@ -1036,8 +1036,8 @@ void PREF_ReaderCallback(void       *closure,
                          PrefValue   value,
                          PrefType    type,
                          bool        isDefault,
-                         bool        isStickyDefault)
-
+                         bool        isStickyDefault,
+                         bool        isLocked)
 {
     uint32_t flags = 0;
     if (isDefault) {
@@ -1049,4 +1049,6 @@ void PREF_ReaderCallback(void       *closure,
         flags |= kPrefForceSet;
     }
     pref_HashPref(pref, value, type, flags);
+    if (isLocked)
+        PREF_LockPref(pref, true);
 }
